953c0196be432e63673125604b900bcb2846670d
temurbek <rajabovtemurbek04@gmail.com>
2024-07-15 23:07:26 +0500

    ChopBot1.0

--- ChopBotTest.py
+++ ChopBotTest.py
@@ -20,17 +20,98 @@ form_router = Router()
 
 
 class Form(StatesGroup):
+    language = State()
+    command = State()
     file = State()
     copy = State()
     pages = State()
     page_per_sheet = State()
 
 
-@form_router.message(Command("print"))
+@form_router.message(CommandStart())
 async def command_start(message: Message, state: FSMContext) -> None:
+    await state.set_state(Form.language)
+    # await message.answer("Choose the language", reply_markup=ReplyKeyboardRemove())
+    await message.answer("Choose the language", reply_markup=ReplyKeyboardMarkup(
+        keyboard=[
+            [
+                KeyboardButton(text="UZB"),
+                KeyboardButton(text="RU"),
+                KeyboardButton(text="ENG")
+            ]
+        ],
+        resize_keyboard=True,
+    ),
+
+                         )
+
+
+@form_router.message(F.text, Form.language)
+async def process_language(message: Message, state: FSMContext) -> None:
+    await state.update_data(lang=message.text)
+    await state.set_state(Form.command)
+    data = await state.get_data()
+    if data['lang'] == "UZB":
+        await message.answer("Tanlang", reply_markup=ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    KeyboardButton(text="/chop"),
+                    KeyboardButton(text="/sozlamalar"),
+                ]
+            ],
+            resize_keyboard=True,
+        ),
+                             )
+    elif data['lang'] == "RU":
+        await message.answer("Выберите команду", reply_markup=ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    KeyboardButton(text="/печатать"),
+                    KeyboardButton(text="/настройки"),
+                ]
+            ],
+            resize_keyboard=True,
+        ),
+                             )
+    elif data['lang'] == "ENG":
+        await message.answer("Choose the command", reply_markup=ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    KeyboardButton(text="/print"),
+                    KeyboardButton(text="/settings"),
+                ]
+            ],
+            resize_keyboard=True,
+        ),
+                             )
+
+
+@form_router.message(Command("print"))
+async def command_print(message: Message, state: FSMContext) -> None:
     await state.set_state(Form.file)
+    await state.update_data(command=message.text)
     await message.answer(
-        "Fileni yuboring",
+        "Send file",
+        reply_markup=ReplyKeyboardRemove(),
+    )
+
+
+@form_router.message(Command("chop"))
+async def command_print(message: Message, state: FSMContext) -> None:
+    await state.set_state(Form.file)
+    await state.update_data(command=message.text)
+    await message.answer(
+        "Faylni yuboring",
+        reply_markup=ReplyKeyboardRemove(),
+    )
+
+
+@form_router.message(Command("печатать"))
+async def command_print(message: Message, state: FSMContext) -> None:
+    await state.set_state(Form.file)
+    await state.update_data(command=message.text)
+    await message.answer(
+        "Выберите файл",
         reply_markup=ReplyKeyboardRemove(),
     )
 
@@ -39,9 +120,19 @@ async def command_start(message: Message, state: FSMContext) -> None:
 async def process_file(message: Message, state: FSMContext) -> None:
     await state.update_data(file=message.document.file_name)
     await state.set_state(Form.copy)
-    await message.answer(
-        f"Nechta nusxa chiqrish kerak?"
-    )
+    data = await state.get_data()
+    if data['lang'] == 'UZB':
+        await message.answer(
+            f"Nusxalar sonini kiriting"
+        )
+    elif data['lang'] == 'RU':
+        await message.answer(
+            f"Введите количество копий"
+        )
+    elif data['lang'] == 'ENG':
+        await message.answer(
+            f"Enter the copy number"
+        )
 
 
 @form_router.message(F.text, Form.copy)
@@ -49,19 +140,38 @@ async def process_copy(message: Message, state: FSMContext) -> None:
     await state.update_data(copy=message.text)
     await state.set_state(Form.pages)
     data = await state.get_data()
-    await message.answer(
-        "Betlarni tanlang",
-        reply_markup=ReplyKeyboardRemove()
-    )
+
+    if data['lang'] == 'UZB':
+        await message.answer(
+            "Betlarni tanlang",
+            reply_markup=ReplyKeyboardRemove()
+        )
+    elif data['lang'] == 'RU':
+        await message.answer(
+            "Выберите страниц",
+            reply_markup=ReplyKeyboardRemove()
+        )
+    elif data['lang'] == 'ENG':
+        await message.answer(
+            "Choose pages",
+            reply_markup=ReplyKeyboardRemove()
+        )
 
 
 @form_router.message(F.text, Form.pages)
 async def process_pages(message: Message, state: FSMContext) -> None:
     await state.update_data(pages=message.text)
     await state.set_state(Form.page_per_sheet)
-    #  data = await state.get_data()
-    await message.answer("Pages per sheet", reply_markup=ReplyKeyboardRemove()
-                         )
+    data = await state.get_data()
+    if data['lang'] == 'UZB':
+        await message.answer("Betdagi varoqlar sonini kiriting", reply_markup=ReplyKeyboardRemove()
+                             )
+    elif data['lang'] == 'RU':
+        await message.answer("Страниц в одном листе", reply_markup=ReplyKeyboardRemove()
+                             )
+    elif data['lang'] == 'ENG':
+        await message.answer("Pages per sheet", reply_markup=ReplyKeyboardRemove()
+                             )
 
 
 @form_router.message(F.text, Form.page_per_sheet)
@@ -69,7 +179,18 @@ async def process_pages(message: Message, state: FSMContext) -> None:
     await state.update_data(pages_per_sheet=message.text)
     data = await state.get_data()
     #await state.clear()
-    await message.answer(f"Check the settings:\nFile: {data['file']},\nCopy: {data['copy']},\nPages: {data['pages']},\nPages per sheet: {data['pages_per_sheet']}")
+    if data['lang'] == 'UZB':
+        await message.answer(
+            f"Tekshiring:\nFayl: {data['file']},\nNusxa soni: {data['copy']},\nBetlar: {data['pages']},\nBir betdagi varoqlar soni: {data['pages_per_sheet']}"
+        )
+    elif data['lang'] == 'RU':
+        await message.answer(
+            f"Проверьте:\nФайл: {data['file']},\nЧисло копий: {data['copy']},\nСтраницы: {data['pages']},\nЧисло страниц на одном листе: {data['pages_per_sheet']}"
+        )
+    elif data['lang'] == 'ENG':
+        await message.answer(
+            f"Check settings:\nFile: {data['file']},\nCopy: {data['copy']},\nPages: {data['pages']},\nPages per sheet: {data['pages_per_sheet']}"
+        )
 
 
 async def main():
